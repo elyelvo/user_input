67fdaa4ee28a8e2f059801e0b61496bc898a4774
elyelvo <eldosampo@gmail.com>
2024-08-30 11:21:49 -0700

    week 1 assignment

---
+++ user_input.py
@@ -0,0 +1,8 @@
+#input  name
+name=input("What is your name?: ")
+#input   age
+age=input("What is your age?:  ")
+#input location
+location=input("Where are you located?: ")
+#prints the output
+print(f"Hello {name}, you are {age} years old and live in {location}.")
